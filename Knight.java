59f193fbc99184751e59e8d1f3f7adba742c826d
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-02-13 00:46:26 +0500

    knight functionality added

--- Knight.java
+++ Knight.java
@@ -13,4 +13,19 @@ public class Knight extends Piece {
 		 	image = getImage("bn");
 		 }
 	}
+
+	public boolean canMove(int targetCol, int targetRow) {
+		
+		if(isWithinBoard(targetCol, targetRow)) {
+			// knight can move if its movement ratio of col and row is 1:2 or 2:1
+			if(Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2)  {
+				if(isValidSquare(targetCol, targetRow)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+
 }
